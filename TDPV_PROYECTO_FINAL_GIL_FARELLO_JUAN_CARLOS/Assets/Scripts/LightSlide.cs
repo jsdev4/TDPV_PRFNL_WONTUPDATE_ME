97cd1fd173ac26f004cb246a4b9cf7ba94f2093a
Juan Speed <jsdev4@users.noreply.github.com>
2020-05-01 20:00:58 -0300

    Added more lights.
    Added more lamp lights, color gradient changes across the level.
    Also added large lamps and a point lights.
    Added scripts for move the point light on the large lamps using objects as "nodes".
    Added a light point which turns on when checkpoint is saved.

---
+++ TDPV_PROYECTO_FINAL_GIL_FARELLO_JUAN_CARLOS/Assets/Scripts/LightSlide.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightSlide : MonoBehaviour
+{
+    public GameObject light_to_slide;
+    private Transform trnsfrm;
+    private Transform this_object;
+    void Start()
+    {
+        this_object = GetComponent<Transform>();
+        trnsfrm = light_to_slide.gameObject.GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (trnsfrm.position.x <this_object.localScale.x)
+        {
+            light_to_slide.GetComponent<Transform>().Translate(Vector3.right*1*Time.deltaTime);
+        }
+        if (trnsfrm.position.x >= this_object.localScale.x)
+        {
+            light_to_slide.GetComponent<Transform>().Translate(this_object.localScale.x*-1,0,0);
+
+        }
+
+    }
+}
