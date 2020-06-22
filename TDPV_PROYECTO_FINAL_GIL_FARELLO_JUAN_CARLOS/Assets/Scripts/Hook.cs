e825381b75974bb3bbe2ce65b26bcc8d938e98e4
Juan Speed <jsdev4@users.noreply.github.com>
2020-06-21 23:28:16 -0300

    Added gui for level02.
    Added gui for level02 and made quick configuration.
    Added 2 enemies for testing it.

--- TDPV_PROYECTO_FINAL_GIL_FARELLO_JUAN_CARLOS/Assets/Scripts/Hook.cs
+++ TDPV_PROYECTO_FINAL_GIL_FARELLO_JUAN_CARLOS/Assets/Scripts/Hook.cs
@@ -5,45 +5,102 @@ using UnityEngine;
 
 public class Hook : MonoBehaviour
 {
+    public int hook_number;
     public bool direction;
     public float speed;
     private Transform trnsfrm;
     public GameObject player;
     private bool player_on_it;
+    public GameObject[] hooks;
     void Start()
     {
         trnsfrm = GetComponent<Transform>();
     }
     void Update()
     {
- 
-        if(direction==true)
-        {
-            trnsfrm.Translate(Vector3.right * speed * Time.deltaTime);
-        }
-       else
-        {
+         Vector3 currentPos = trnsfrm.position;
+         Vector3 currentPosfixed = new Vector3(currentPos.x, currentPos.y - 2f, currentPos.z);
+         if (direction == true)
+         {
+             trnsfrm.Translate(Vector3.right * speed * Time.deltaTime);
+         }
+         else
+         {
             trnsfrm.Translate(Vector3.left * speed * Time.deltaTime);
-        }
-       if(player_on_it==true)
-        {
-            player.gameObject.GetComponent<Transform>().position = transform.position;
-            player.gameObject.GetComponent<Transform>().Translate(Vector3.down * 1);
-            if(Input.GetKeyUp(KeyCode.F))
+         }
+         if (hook_number==0&&player_on_it == true)
+         {
+            hooks[1].SetActive(false);
+            hooks[2].SetActive(false);
+            hooks[3].SetActive(false);
+            player.gameObject.GetComponent<CharController>().Set_if_is_on_the_hook(true);
+            player.gameObject.GetComponent<Rigidbody>().MovePosition(currentPosfixed);
+            if (Input.GetKeyUp(KeyCode.F))
             {
                 player_on_it = false;
             }
+            if (Input.GetKeyUp(KeyCode.Space) && player.gameObject.GetComponent<CharController>().Return_if_is_fffffflipped() == true)
+            {  
+                player.gameObject.GetComponent<Rigidbody>().AddForce(Vector3.right * 7.5f, ForceMode.Impulse);        
+                player_on_it = false;   
+            }
+            if (Input.GetKeyUp(KeyCode.Space) && player.gameObject.GetComponent<CharController>().Return_if_is_fffffflipped() == false)
+            {
+                player.gameObject.GetComponent<Rigidbody>().AddForce(Vector3.left * 7.5f, ForceMode.Impulse);
+                player_on_it = false; 
+            }        
+        }      
+        if (hook_number==0&&player_on_it == false)  
+        {       
+            player.gameObject.GetComponent<CharController>().Set_if_is_on_the_hook(false);
+            hooks[1].SetActive(true);
+            hooks[2].SetActive(true);
+            hooks[3].SetActive(true);
+        }
+        if (hook_number==1 && player_on_it == true)
+        {
+            hooks[0].SetActive(false);
+            hooks[2].SetActive(false);
+            hooks[3].SetActive(false);
+            player.gameObject.GetComponent<CharController>().Set_if_is_on_the_hook(true);
+            player.gameObject.GetComponent<Rigidbody>().MovePosition(currentPosfixed);
+            if (Input.GetKeyUp(KeyCode.F))
+            {
+                player_on_it = false;
+            }
+            if (Input.GetKeyUp(KeyCode.Space) && player.gameObject.GetComponent<CharController>().Return_if_is_fffffflipped() == true)
+            {
+                player.gameObject.GetComponent<Rigidbody>().AddForce(Vector3.right * 7.5f, ForceMode.Impulse);
+                player_on_it = false;
+            }
+            if (Input.GetKeyUp(KeyCode.Space) && player.gameObject.GetComponent<CharController>().Return_if_is_fffffflipped() == false)
+            {
+                player.gameObject.GetComponent<Rigidbody>().AddForce(Vector3.left * 7.5f, ForceMode.Impulse);
+                player_on_it = false;
+            }   
+            
+        }
+        if (hook_number==1&&player_on_it == false)
+        {
+            player.gameObject.GetComponent<CharController>().Set_if_is_on_the_hook(false);
+            hooks[0].SetActive(true);
+            hooks[2].SetActive(true);
+            hooks[3].SetActive(true);
         }
     }
     private void OnCollisionEnter(Collision collision)
     {
-        if(collision.collider.CompareTag("HookCollider"))
+        if (collision.collider.CompareTag("HookCollider"))
         {
             direction = !direction;
         }
-        if(collision.collider.CompareTag("Player"))
+        if (collision.collider.CompareTag("Player"))
         {
             player_on_it = true;
         }
     }
+    public void set_if_player_is_on_it(bool on_it)
+	{
+        player_on_it = on_it;
+	}
 }
---
+++ TDPV_PROYECTO_FINAL_GIL_FARELLO_JUAN_CARLOS/Assets/Scripts/ObjectRotorController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectRotorController : MonoBehaviour
+{
+    public float rotation_angle;
+    private Transform trnsfrm;
+    void Start()
+    {
+        trnsfrm = GetComponent<Transform>();
+    }
+    void Update()
+    {
+        trnsfrm.Rotate(0, rotation_angle * Time.deltaTime, 0);
+    }
+}
