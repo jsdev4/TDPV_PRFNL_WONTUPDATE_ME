2ffc68cff4b78913deefde2e1b3ad793a2148af4
Juan Speed <jsdev4@users.noreply.github.com>
2020-07-19 10:31:47 -0300

    Added elements.
    Added prefab lamps. also added lots of lamps on the level.
    added glass materials.
    added desktops.
    added computers with their own animations.
    added a prefab chair with their own textures.
    added cups of coffee.
    added background and changed the direction which our player gives when he finishes the level.

---
+++ TDPV_PROYECTO_FINAL_GIL_FARELLO_JUAN_CARLOS/Assets/Scripts/IntermitentLight.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntermitentLight : MonoBehaviour
+{
+	private bool is_shinning;
+	public float delay;
+	private float time_elapsed;
+	private Light lamp;
+	public float increase_intensity;
+	//public float max_intensity;*/
+	void Start()
+	{
+		is_shinning = false;
+		lamp = GetComponent<Light>();
+
+	}
+
+	void Update()
+	{
+		if(is_shinning==false)
+		{
+			time_elapsed += Time.deltaTime;
+			if(time_elapsed<=delay)
+			{
+				lamp.intensity += increase_intensity * Time.deltaTime;
+			}
+			else
+			{
+				time_elapsed = 0;
+				is_shinning = true;
+			}
+		}
+		if(is_shinning==true)
+		{
+			time_elapsed += Time.deltaTime;
+			if(time_elapsed<=delay)
+			{
+				lamp.intensity -= increase_intensity * Time.deltaTime;
+			}
+			else
+			{
+				time_elapsed = 0;
+				is_shinning = false;
+			}
+		}
+
+		
+	}
+
+	
+}
