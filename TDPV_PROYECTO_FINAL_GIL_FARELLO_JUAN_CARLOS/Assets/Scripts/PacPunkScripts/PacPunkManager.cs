3e218ab6d67f01ae634b8ad53269e5d532f022a6
Juan Speed <jsdev4@users.noreply.github.com>
2020-08-06 13:25:41 -0300

    elements for mini level.
    Added background.
    added lights as prefabs.
    added area lights wich were baked.
    added more textures for floors and walls.
    added all the items.

--- TDPV_PROYECTO_FINAL_GIL_FARELLO_JUAN_CARLOS/Assets/Scripts/PacPunkScripts/PacPunkManager.cs
+++ TDPV_PROYECTO_FINAL_GIL_FARELLO_JUAN_CARLOS/Assets/Scripts/PacPunkScripts/PacPunkManager.cs
@@ -9,34 +9,41 @@ public class PacPunkManager : MonoBehaviour
     public float critical_time;
     private int icons_collected;
     private bool special_icon_collected;
+    private int numberOfTaggedObjects;
     void Start()
     {
+        numberOfTaggedObjects = GameObject.FindGameObjectsWithTag("WaitingIconBall").Length;
         timer = 0;
         special_icon_collected = false;
         icons_collected = 0;
+        Debug.Log(numberOfTaggedObjects);
     }
 
     // Update is called once per frame
     void Update()
     {
         if(special_icon_collected==true)
-		{
+		{//enemies are good
             timer += Time.deltaTime;
             //call to enemies status bad or good
             if (timer >= critical_time)
 			{
-
+                //enemies are good
                 //enemies flashing;
 			}
-            if(timer>=max_time)
-			{
+            if (timer >= max_time)
+            {
                 //enemies back to normal
                 //enemies are bad
                 Debug.Log("Special icon is out");
                 timer = 0;
                 special_icon_collected = false;
-			}
-
+            }
+		}
+        if(icons_collected==numberOfTaggedObjects)
+		{
+            //victory condition
+            Debug.Log("all the objects were collected");
 		}
     }
     public void Increase_icons_collected()
